8133465532836239ea819b83245cf9a377dd246d
umerzaidi <63396185+umerzaidi@users.noreply.github.com>
2023-06-29 00:20:23 +0500

    Final

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ newFunction();
 
 function newFunction() {
   document.addEventListener('DOMContentLoaded', function () {
-    fetch('en.json')
+    fetch('https://labs.j-novel.club/app/v1/series/ascendance-of-a-bookworm/volumes?format=json')
       .then(response => response.json())
       .then(data => {
         const booksContainer = document.getElementById('books');
-        const books = data.pageProps.data.titlesLatest;
-
+        const books = data.volumes;
         books.forEach(book => {
-          if (book.credits.author === 'Miya Kazuki') {
             const bookElement = document.createElement('div');
             bookElement.className = 'book';
+            
 
             const titleElement = document.createElement('h2');
             titleElement.className = 'name';
-            titleElement.textContent = book.name;
+            titleElement.textContent = book.title;
 
             const imageElement = document.createElement('img');
-            imageElement.src = book.thumbnail;
+            imageElement.src = book.cover.coverUrl;
             imageElement.alt = 'Book Cover';
 
             const descriptionElement = document.createElement('p');
@@ -28,14 +27,14 @@ function newFunction() {
 
             const authorElement = document.createElement('p');
             authorElement.className = 'auth';
-            authorElement.textContent = 'Author: ' + book.credits.author;
+            authorElement.textContent = 'Author: Miya Kazuki';
 
             bookElement.appendChild(titleElement);
             bookElement.appendChild(imageElement);
             bookElement.appendChild(descriptionElement);
             bookElement.appendChild(authorElement);
             booksContainer.appendChild(bookElement);
-          }
+          
         });
       })
       .catch(error => console.log(error));
